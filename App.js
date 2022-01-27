9c7caec3cdcba885bae215555093df9076661d85
thingsjasmade <98535796+thingsjasmade@users.noreply.github.com>
2022-01-27 09:20:00 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todoform from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
